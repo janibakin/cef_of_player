5798acb21fecd5b38bcb9bc56d9da7b9a4c0c9be
Zhanibek Bakin <zhanibek.bakin@gmail.com>
2025-06-17 20:14:38 +0100

    Clean up of Bazel and fix some includes

--- simpleapp/simple_handler_linux.cc
+++ simpleapp/simple_handler_linux.cc
@@ -2,7 +2,7 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
-#include "tests/cefsimple/simple_handler.h"
+#include "simple_handler.h"
 
 #if defined(CEF_X11)
 #include <X11/Xatom.h>
